9834ac508a26e86dd86103d1df52bbfc9087fa1c
naveenairani <naveenairani65@gmail.com>
2025-04-10 20:30:50 +0530

    initial commit

---
+++ src/main/java/com/byterains/forex/platform/forex_platform/ForexPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.byterains.forex.platform.forex_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byterains/forex/platform/forex_platform/ForexPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byterains.forex.platform.forex_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
